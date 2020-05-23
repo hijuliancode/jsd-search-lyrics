bcfb3171a24ee3412dbfe8199083a9b4ff666ee0
JulianCode <hijuliansosa@protonmail.com>
2020-05-23 13:58:12 -0500

    Validating the search form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 import * as UI from './interfaz.js'
 
-console.log(UI)
+UI.formularioBuscar.addEventListener('submit', e => {
+  e.preventDefault()
+
+  // Obtener valores del formulario
+  const artista = document.querySelector('#artista').value,
+        cancion = document.querySelector('#cancion').value
+
+  if(artista === '' || cancion === '') { // Formulario con campos vacios
+    UI.divMensajes.innerHTML = 'Error => Todos los campos son obligatorios'
+    UI.divMensajes.classList.add('error')
+    setTimeout(() => {
+      UI.divMensajes.innerHTML = ''
+    UI.divMensajes.classList.remove('error')
+    }, 3000)
+  } else { // Formulario completo, realizar consulta a la API
+
+  }
+})
